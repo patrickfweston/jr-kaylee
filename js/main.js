3eff2d27d03aef487be536b80237f725ce8a5d7a
patrickfweston <patrickfweston@gmail.com>
2014-08-09 15:28:44 -0400

    Updated stylesheet changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+
   $('.navigation__hamburger').click(function(e) {
     e.preventDefault();
     $('.navigation__wrapper').toggleClass('isDisplayed');
@@ -12,9 +14,26 @@ $(document).ready(function() {
     }
 
     var id = $(this).attr("href");
+    var offset = 0;
+    if ($('.navigation').css('position') === 'fixed') {
+      offset = $('.navigation').height();
+    }
     $('body, html').animate({
-      scrollTop: $(id).offset().top
+      scrollTop: $(id).offset().top - offset
     });
 
   })
+});
+
+$(window).load(function() {
+    $('.slider').flexslider({
+      selector: ".slider__wrapper > li",
+      controlNav: false,
+      prevText: "",
+      nextText: "",
+      controlsContainer: ".slider__controls"
+    });
+
+    $('.flex-prev').addClass('fontawesome-angle-left');
+    $('.flex-next').addClass('fontawesome-angle-right');
 });
